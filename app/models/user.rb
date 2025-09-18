0474e7e381da6216ca126463b376d16fda5f7f7f
UmeReds0729 <ume.reds-isotope@nifty.com>
2025-09-19 00:25:14 +0900

    #2管理画面実装：ActiveAdminおよびDevise導入

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+end
