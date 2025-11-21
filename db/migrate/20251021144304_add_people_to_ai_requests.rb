ddbea4ff7176576a2fe408f6d747214d64359552
UmeReds0729 <ume.reds-isotope@nifty.com>
2025-11-21 22:38:19 +0900

    #4メニューと食材のCRUD+中間テーブル：不要なマイグレーションファイルを削除

--- db/migrate/20251021144304_add_people_to_ai_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddPeopleToAiRequests < ActiveRecord::Migration[7.1]
-  def change
-    add_column :ai_requests, :people, :integer
-  end
-end
--- db/migrate/20251021151524_add_ai_request_to_menus.rb
+++
@@ -1,5 +0,0 @@
-class AddAiRequestToMenus < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :menus, :ai_request, null: false, foreign_key: true
-  end
-end
